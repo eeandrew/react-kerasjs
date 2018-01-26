04cf53c222d04e3a1276159584598a48ab67acdf
eeandrew <boyzhoulin@sina.com>
2018-01-26 16:06:26 +0800

    [fea]webgl add

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React,{
 import ReactDOM from 'react-dom';
 import MainComponent from './test'
 import IdentifySanta from './identify_santa/index'
+import {testAdd} from './test/webgl/dynamic/merge/add'
 
 const render = Component => {
   ReactDOM.render(<Component/>,document.getElementById('app'))
---
+++ src/test/webgl/dynamic/merge/add.js
@@ -0,0 +1,5 @@
+import add from '../../../../webgl/dynamic/merge/add'
+
+export function testAdd() {
+  console.log(add(2,[3,4]))
+}
